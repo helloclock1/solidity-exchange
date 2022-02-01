4fb3fb5632a5826164f56be84ecf32eb426961f4
Berezin Nikolai <87578581+York3r@users.noreply.github.com>
2022-02-02 01:08:38 +0300

    Create beat_coin.sol

---
+++ beat_coin.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.7;
+
+contract DodgeCoin{
+    string constant name = "BeatCoin";
+    string constant symbol = "BC";
+    uint8 constant decimals = 10;
+    uint totalSupply;
+    address owner;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    function myBalance( address adr ) public view returns(uint){
+        return balances[adr];
+    }
+    function balanceOf(address adr) public view returns(uint){
+        return balances[adr];
+    }
+
+    function mint(address adr, uint value) public onlyOwner{
+        totalSupply += value;
+        balances[adr] += value;
+    }
+
+    function transferTo(address from, address to, uint tokens) public{
+        require(balances[from] >= tokens);
+        balances[from] -= tokens;
+        balances[to] += tokens;
+    }
+
+}
