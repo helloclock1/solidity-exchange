d6dbbbad4e05101abacf138bf4c00322326fefe9
Berezin Nikolai <87578581+York3r@users.noreply.github.com>
2022-02-02 01:09:26 +0300

    Update exchange.sol

--- exchange.sol
+++ exchange.sol
@@ -1,16 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.7;
 
-contract DodgeCoin{
-    string constant name = "DodgeCoin";
-    string constant symbol = "DC";
-    uint8 constant decimals = 10;
-    uint totalSupply;
-    address owner;
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+interface iDodgeCoin{
+    function balanceOf(address adr) external view returns(uint);
+    function transferTo(address from, address to, uint tokens) external payable;  
+}
 
+interface iBeatCoin{
+    function balanceOf(address adr) external view returns(uint);
+    function transferTo(address from, address to, uint tokens) external payable;  
+}
+
+contract Exchange{
+    address owner;
+    uint8 current_coin;
+    // 1 - DodgeCoin, 2 - BeatCoin
+
+    struct offer{
+        address owner;
+        uint8 tokenIn;
+        uint8 tokenOut;
+        uint8 amountOut;
+        int ratio; // получается соотношение курса входящего токена к исходящему. так например если выставить 5 токенов с рейтом 3,  
+        // они обменяются на 5 * 3 монет, т.е. 15
+    }
+
+    iDodgeCoin DodgeCoin;
+    iBeatCoin BeatCoin;
+    
     constructor(){
+        current_coin = 1;
+        DodgeCoin = iDodgeCoin(0xf428f3281b45E1CA109Cac1072b476706D170CCc);
         owner = msg.sender;
     }
 
@@ -19,22 +39,36 @@ contract DodgeCoin{
         _;
     }
 
-    function myBalance( address adr ) public view returns(uint){
-        return balances[adr];
-    }
-    function balanceOf(address adr) public view returns(uint){
-        return balances[adr];
+    function get_current_coin() public view returns(string memory){
+        if (current_coin == 1){
+            return "DodgeCoin";
+        }
+        else if (current_coin == 2){
+            return "BeatCoin";
+        }
+        else{
+            return "Error occured";
+        }
     }
 
-    function mint(address adr, uint value) public onlyOwner{
-        totalSupply += value;
-        balances[adr] += value;
+    function select_DodgeCoin() public{
+        current_coin = 1;
     }
 
-    function transferTo(address from, address to, uint tokens) public{
-        require(balances[from] >= tokens);
-        balances[from] -= tokens;
-        balances[to] += tokens;
+    function select_BeatCoin() public{
+        current_coin = 2;
+    }
+
+    function DC_Transfer(address adr, uint tokens) public{
+        DodgeCoin.transferTo(msg.sender, adr, tokens);
+    }
+
+    function DC_BalanceOf()public view returns(uint256){
+        return DodgeCoin.balanceOf(msg.sender);
+    } 
+
+    function DC_BalanceOf(address adr) public view returns(uint256){
+        return DodgeCoin.balanceOf(adr);
     }
 
 }
