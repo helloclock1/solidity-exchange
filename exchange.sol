8d9821ca2ae7a23d882d2a885775f51927532080
Berezin Nikolai <87578581+York3r@users.noreply.github.com>
2022-02-03 01:56:37 +0300

    Offer creation and token functions optimization

--- exchange.sol
+++ exchange.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.7;
+import "./offer.sol";
 
 interface iDodgeCoin{
     function balanceOf(address adr) external view returns(uint);
@@ -16,21 +17,15 @@ contract Exchange{
     uint8 current_coin;
     // 1 - DodgeCoin, 2 - BeatCoin
 
-    struct offer{
-        address owner;
-        uint8 tokenIn;
-        uint8 tokenOut;
-        uint8 amountOut;
-        int ratio; // получается соотношение курса входящего токена к исходящему. так например если выставить 5 токенов с рейтом 3,  
-        // они обменяются на 5 * 3 монет, т.е. 15
-    }
+    address[] offers;
 
     iDodgeCoin DodgeCoin;
     iBeatCoin BeatCoin;
     
     constructor(){
         current_coin = 1;
-        DodgeCoin = iDodgeCoin(0xf428f3281b45E1CA109Cac1072b476706D170CCc);
+        DodgeCoin = iDodgeCoin(0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3);
+        BeatCoin = iBeatCoin(0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99);
         owner = msg.sender;
     }
 
@@ -50,7 +45,7 @@ contract Exchange{
             return "Error occured";
         }
     }
-
+    
     function select_DodgeCoin() public{
         current_coin = 1;
     }
@@ -63,12 +58,46 @@ contract Exchange{
         DodgeCoin.transferTo(msg.sender, adr, tokens);
     }
 
-    function DC_BalanceOf()public view returns(uint256){
+    function DC_BalanceOf()internal view returns(uint256){
         return DodgeCoin.balanceOf(msg.sender);
     } 
 
-    function DC_BalanceOf(address adr) public view returns(uint256){
+    function DC_BalanceOf(address adr) internal view returns(uint256){
         return DodgeCoin.balanceOf(adr);
     }
+
+    function BC_BalanceOf()internal view returns(uint256){
+        return BeatCoin.balanceOf(msg.sender);
+    } 
+
+    function BC_BalanceOf(address adr) internal view returns(uint256){
+        return BeatCoin.balanceOf(adr);
+    }
+
+    function getBalanceByToken(address adr, uint8 token)internal returns(uint256){
+        if (token == 1){
+            return DC_BalanceOf(adr);
+        }
+        else if (token == 2){
+            return BC_BalanceOf(adr);
+        }
+    }
+
+    function createOffer(uint8 _tokenIn, uint8 _tokenOut,uint8 _amountIn, uint8 _amountOut)public returns(address){ 
+        require(( 0 < _tokenIn) && (_tokenIn < 3));
+        require(( 0 < _tokenOut) && (_tokenOut < 3));
+        require(getBalanceByToken(msg.sender, _tokenOut) >= _amountOut);
+
+        Offer offer = new Offer(msg.sender, _tokenIn, _tokenOut, _amountIn,  _amountOut);
+        offers.push(address(offer));
+        return address(offer);
+    }
+
+
+    // пока заморожено, легче смотреть баланс через MetaMask
+    // function getMyBalance()public returns(uint256){
+    //     uint256 balance = getBalanceByToken(msg.sender, current_coin);
+    //     return balance;
+    // }
 
 }
